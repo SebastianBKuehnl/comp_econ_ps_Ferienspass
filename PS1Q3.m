bcea8b56d615dcb173ce08341aa237e4d49e19a3
SebastianBKuehnl <sebastian.kuehnl@gmx.de>
2017-12-21 17:46:35 +0100

    Working
    Working, Border-Behavior still missing

--- PS1Q3.m
+++ PS1Q3.m
@@ -4,7 +4,7 @@ clear all;
 clc;
 
 Board=zeros(15,15); 
-
+Iteration_Count=0;
 %110 White (1 indicates white)
 i=1;
 while i<110
@@ -33,8 +33,10 @@ end
 end
 
 figure
+subplot(2,2,1)
 Plot_0=imagesc(Board);
 
+for ij=1:45
 %% Spot unoccupied houses
 
 
@@ -60,12 +62,16 @@ end
 %% Evaluate position
 
 Movers_Location=zeros(15,15);
+Movers_Count=0;
 
 for i=1:15
     for j=1:15
        Self=Board(i,j);
        %Evaluate at Borders
-       if i==1||i==1||i==15||j==15
+       if i==1||j==1||i==15||j==15
+           
+          % if i==1
+          %Elseif for every border possibility     
            
        else
        %Evaluate interior
@@ -92,7 +98,8 @@ for i=1:15
            Neigh_Eval=sum(Neigh)/4;
            
           if Neigh_Eval<0.35
-              Movers_Location(i,j)=1;
+              Movers_Location(i,j)=1; %everyone with a one wants to move
+              Movers_Count=Movers_Count+1;
           end
            
        
@@ -100,6 +107,80 @@ for i=1:15
     end
 end
 
+%% Only five households are allowed to move each period (since only five houses are unoccupied)
 
+Allowed_Movers=zeros(5,1); 
+Allowed_Movers_Pos=zeros(5,2);
+Want_to_Move=zeros(Movers_Count,2);
+k=1;
+for i=1:15
+    for j=1:15
+       if Movers_Location(i,j)==1 
+           %Store to movers
+           
+      
+           Want_to_Move(k,1)=i;
+           Want_to_Move(k,2)=j;
+           
+           k=k+1;
+       end
+    end
+end
 
+%Pick five movers
 
+for i=1:5
+Allowed_Movers(i,1) = randi(Movers_Count);
+end
+
+for i=1:5
+    Allowed_Movers_Pos(i,1)=Want_to_Move(Allowed_Movers(i,1),1);
+    Allowed_Movers_Pos(i,2)=Want_to_Move(Allowed_Movers(i,1),2);
+end
+
+%% Let the five households move to a random unoccupied house
+
+Moves_To=zeros(5,2);
+Unoccupied_NowOccupied=zeros(5,1);
+i=1;
+
+while i<6
+    
+    Row=randi(5);
+    if ismember(Row,Unoccupied_NowOccupied)==0
+    Unoccupied_NowOccupied(i,1)=Row;
+     
+    Moves_To(i,1)=Non_Occupied(Row,1);
+    Moves_To(i,2)=Non_Occupied(Row,2);
+    i=i+1;
+    end
+    
+end
+
+%Set new location of selected movers to zero, set unoccupied houses to new
+%values
+
+for i=1:5
+    Board(Non_Occupied(i,1),Non_Occupied(i,2))=Board(Allowed_Movers_Pos(i,1),Allowed_Movers_Pos(i,2));
+    Board(Allowed_Movers_Pos(i,1),Allowed_Movers_Pos(i,2))=0;
+end
+
+Iteration_Count=Iteration_Count+1;
+Title= ['Iteration' num2cell(Iteration_Count)];
+
+if ij==15
+    subplot(2,2,2)
+    Plot_1=imagesc(Board);
+    title(Title)
+elseif ij== 30
+    subplot(2,2,3)
+    Plot_2=imagesc(Board);
+    title(Title)
+elseif ij== 45
+    subplot(2,2,4)
+    Plot_3=imagesc(Board);
+    title(Title)
+        
+end
+        
+end
