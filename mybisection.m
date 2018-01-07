39ba39d29965343f23b876dc3267e89c77be85e1
Alex <reebyte.go@gmail.com>
2018-01-07 10:26:27 +0100

    Update mybisection.m
    requirement check added

--- mybisection.m
+++ mybisection.m
@@ -6,6 +6,8 @@ function [ r ] = mybisection( fun, a, b, maxiter, eps_step, eps_abs )
   
   
   % Check requirements: 
+  if (maxiter<=0) || (eps_step<0) || (eps_abs<0)
+    error('input parameters invalid');
   %% fun at a or b already zero? 
   if ( fun(a) == 0 )
     r = a;
