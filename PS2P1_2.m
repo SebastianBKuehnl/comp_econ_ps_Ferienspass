eb6125e5de3b30dff556948ad43e9a4fee8a8225
Alex <reebyte.go@gmail.com>
2018-01-07 11:04:48 +0100

    Update PS2P1_2.m
    full update to mybisection and readability improvement as well as excluding further zeros in a sufficiently large interval.

--- PS2P1_2.m
+++ PS2P1_2.m
@@ -11,23 +11,43 @@ figure
 plot(h,'--r');
 title('Function 2');
 %do bisection
+maxiter=20;
 e=0.0001;
 d=0.0001;
 x_l=-5;
 x_h=0.7;
 fun=@ffunction;
-a=mybisec(fun,x_l,x_h,e,d);
-disp(a)
+a=mybisection(fun,x_l,x_h,maxiter,e,d);
+disp(a)      %-1.6632
 x_l=0.01;
 x_h=0.7;
-b=bisec(x_l,x_h,e,fun);
-disp(b)
+b=mybisection(fun,x_l,x_h,maxiter,e,d);
+disp(b)      %0.2490
+%x_l=0.25;
+%x_h=100;
+%l=mybisection(fun,x_l,x_h,maxiter,e,d);
+%disp(l)    %(error: not opposite signs)
+%x_l=-50;
+%x_h=-1.7;
+%l=mybisection(fun,x_l,x_h,maxiter,e,d);
+%disp(l)    %(error: not opposite signs) --> no more zeros
+%function 1 has zeros at -1.6632 and 0.2490
+%function 2
 x_l=0.1;
 x_h=100;
 fan=@fffunction;
-c=mybisec(fan,x_l,x_h,e,d);
-disp(c)
+c=mybisection(fan,x_l,x_h,maxiter,e,d);
+disp(c)     %1
 x_l=0;
 x_h=100;
-d=bisec(x_l,x_h,e,fun);
-disp(d)
+t=mybisection(fan,x_l,x_h,maxiter,e,d);
+disp(t)     %0
+%x_l=-0.1;
+%x_h=-20;
+%s=mybisection(fan,x_l,x_h,maxiter,e,d);
+%disp(s)    (error: not opposite signs)
+%x_l=1.1;
+%x_h=100;
+%v=mybisection(fan,x_l,x_h,maxiter,e,d);
+%disp(v)    (error: not opposite signs)  --> no more zeros
+%function 2 has zeros at 0 and 1
