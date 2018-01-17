72ffe07026c3d08bd752fdfcc61257554d5b42f1
Alex <reebyte.go@gmail.com>
2018-01-17 14:58:06 +0100

    updated
    theta=1 included
    todo: what if c<=0?

--- PS3P4.m
+++ PS3P4.m
@@ -5,7 +5,7 @@ clear;
 close all;
 clc;
 T=20;
-theta=0.8;
+theta=1;
 %variable initialization (fixed)
 time=linspace(0,T,T+1); %just for plot
 w=zeros(T+1,1);
@@ -18,11 +18,17 @@ c=zeros(T+1,1);
 %computation (could also be written as a function)
 num=zeros(T+1,1);
 denom=zeros(T+1,1);
+if theta==1
+    factor=beta*(1+r);
+else
+    factor=(1+r)^((1-theta)/theta)*beta^(1/theta);
+end
 for i=0:T
   num(i+1)=w(i+1)*(1+r)^(-i);  
-  denom(i+1)=((1+r)^((1-theta)/theta)*beta^(1/theta))^i;  
+  denom(i+1)=(factor)^i;  
 end
 c(1)=sum(num)/sum(denom);
+u=zeros(T,1);
 for i=1:T
   c(i+1)=c(i)*(beta*(1+r))^(1/theta);  
   a(i+1)=a(i)*(1+r)+w(i)-c(i);
@@ -30,6 +36,9 @@ end
 figure
 plot(time,c,time,w,time,a);
 legend('C','W','A')
+figure
+plot(u)
+legend('U')
 acheck=a(end);
 ccheck=c(end);
 if (round(ccheck*1000)/1000==round(acheck*(1+r)*1000)/1000)
