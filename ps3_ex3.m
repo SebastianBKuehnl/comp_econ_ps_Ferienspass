43435ef60bc47e1b4384231219150e030eade106
Paddy <patrick-blank@onlinehome.de>
2018-01-21 22:59:48 +0100

    Update

--- ps3_ex3.m
+++ ps3_ex3.m
@@ -30,12 +30,52 @@ maxiter = 200;
 tol = 0.00001;
 % (a) Newton
 [beta, iter, t] = mynewton(t, X, beta0, maxiter, tol);
+beta
+iter
+t
 
 % (b) Broyden's mthod
-
+[beta, iter, t] = broyden(t, X, beta0, maxiter, tol);
+beta
+iter
+t
 
 % (c) Fixed point iteration
+[beta, iter, t] = fixedpoint(t, X, beta0, maxiter, tol);
+beta
+iter
+t
 
-% (d)
-fun = @ps3ex3foc;
-beta = fsolve(fun, beta0);
+% (d) fsolve
+
+%% 3.6 see 3.5 (b)
+
+%% 3.7 Standardization
+M = max(X);
+m = min(X);
+maxmin = M - m;
+
+for j = 1:nrow1
+    % Standardize lgdp85adult
+    X(j,2) = ( X(j,2) - m(2) )/maxmin(2);
+    % Standardize lschool
+    X(j,3) = ( X(j,3) - m(3) )/maxmin(3);
+end
+
+beta0 = [-10; 10; 1];
+[beta, iter, t] = broyden(t, X, beta0, maxiter, tol);
+beta
+iter
+t
+
+%% 3.8
+% (a) Newton's method
+[beta, iter, t] = broyden(t, X, beta0, maxiter, tol);
+beta
+iter
+t
+
+%% 3.9 tic-toc is already implemented above
+
+
+
