cde578143290ca67f5a323b18d25322528356452
Paddy <patrick-blank@onlinehome.de>
2018-02-04 17:13:16 +0100

    Final version ex. 2

--- ps4ex2_new.m
+++ ps4ex2_new.m
@@ -31,29 +31,88 @@ fimplicit(nonlinMU, [Wmin Wmax 0 30])
 
 %% Interpolation of quadratic utility using Chebyshev
 x=linspace(Wmin,Wmax,1000);
-[ylin, ftilde1, yhat1] = chebyshev_approx(linMU, Wmin, Wmax, m, n, 'explicit',x');
+[ylin, ftilde1, yhat1] = chebyshev_approx(linMU, Wmin, Wmax, m, n, 'explicit', x');
 
 %% Interpolation of CRRA utility using Chebyshev
-[ynonlin, ftilde2, yhat2] = chebyshev_approx(nonlinMU, Wmin, Wmax, m, n, 'implicit',x');
-ynonlin=ynonlin';  %it gives 1X1000 matrix instead of 1000X1 (?!)
+[ynonlin, ftilde2, yhat2] = chebyshev_approx(nonlinMU, Wmin, Wmax, m, n, 'implicit', x');
+ynonlin=ynonlin';  % it gives 1X1000 matrix instead of 1000X1 (?!)
 
-%Plot residuals
-figure
+%% Plot residuals
+figure(1)
 plot(x',ftilde1-ylin,x',ftilde2-ynonlin,'--r')
 xlabel('W')
 ylabel('residuals')
 legend('linear residuals','nonlinear residuals')
-title('Ferienspaß im Semester')
+title('Approximation errors: Quadratic vs. CRRA. Baseline')
+% Accuracy
+acclin = max(abs(ftilde1-ylin));
+fprintf( 'Approximation error*e+13 for quadratic utility: %.4f \n', acclin*10^13)
+accnonlin = max(abs(ftilde2-ynonlin));
+fprintf( 'Approximation error*e+13 for CRRA utility: %.4f \n', accnonlin*10^13)
+% Maximum percentage deviation
+maxdev = max(abs(ynonlin - ylin)./ylin);
+fprintf(  'The maximum percentage deviation is %.2f percent \n', maxdev*100)
 
-figure
+figure(2)
 plot(x',ftilde1,x',ftilde2,'--r',x',ylin,'.b',x',ynonlin,':p')
 xlabel('W')
 ylabel('fcts')
 legend('linear aprox','nonlinear approx','lin fct','nonlin fct')
-title('Ferienspaß im Semester')
+title('Chebyshev approximation: Quadratic vs. CRRA. Baseline')
 
-function [yact , yapp, yhat] = chebyshev_approx( fun, a, b, m, n, funtype,x )
-% function [coeff, xhat, yhat] = chebyshev_approx( fun, a, b, m, n )
+% What happens if setting is changed?
+%% (i) Increase in gamma
+gamma = 4;
+nonlinMUgg = @(W, C0) ( prob(1).*( ( W.*R(1) - C0 ).^-gamma) + prob(2).*( ( W.*R(2) - C0 ).^-gamma) ).^-(1./gamma) - C0;
+[ynonlingg, ftilde2gg, yhat2gg] = chebyshev_approx(nonlinMUgg, Wmin, Wmax, m, n, 'implicit', x');
+ynonlingg=ynonlingg';
+
+% Accuracy
+accnonlin = max(abs(ftilde2gg-ynonlingg));
+fprintf( '(i) Increase in gamma. For example, set gamma = %.2f \n', gamma)
+fprintf( 'Approximation error*e+13 for CRRA utility: %.4f \n', accnonlin*10^13)
+% Maximum percentage deviation
+maxdev = max(abs(ynonlingg - ylin)./ylin);
+fprintf(  'The maximum percentage deviation is %.2f percent \n', maxdev*100)
+
+%% (ii) Decrease in p
+gamma = 2;
+p = 0.2;
+prob = [p 1-p]';
+nonlinMUpp = @(W, C0) ( prob(1).*( ( W.*R(1) - C0 ).^-gamma) + prob(2).*( ( W.*R(2) - C0 ).^-gamma) ).^-(1./gamma) - C0;
+[ynonlinpp, ftilde2pp, yhat2pp] = chebyshev_approx(nonlinMUpp, Wmin, Wmax, m, n, 'implicit', x');
+ynonlinpp=ynonlinpp';
+
+% Accuracy
+accnonlin = max(abs(ftilde2pp-ynonlinpp));
+fprintf( '(ii) Decrease in p. For example, set p = %.2f \n', p)
+fprintf( 'Approximation error*e+13 for CRRA utility: %.4f \n', accnonlin*10^13)
+% Maximum percentage deviation
+maxdev = max(abs(ynonlinpp - ylin)./ylin);
+fprintf(  'The maximum percentage deviation is %.2f percent \n', maxdev*100)
+
+
+%% (iii) Increase spread
+p = 0.5;
+prob = [p 1-p]';
+inc = 0.2;
+rmin = rmin - inc;
+rmax = rmax + inc;
+R = [1+rmin 1+rmax]';
+nonlinMUsp = @(W, C0) ( prob(1).*( ( W.*R(1) - C0 ).^-gamma) + prob(2).*( ( W.*R(2) - C0 ).^-gamma) ).^-(1./gamma) - C0;
+[ynonlinsp, ftilde2sp, yhat2sp] = chebyshev_approx(nonlinMUsp, Wmin, Wmax, m, n, 'implicit', x');
+ynonlinsp=ynonlinsp';
+
+% Accuracy
+accnonlin = max(abs(ftilde2sp-ynonlinsp));
+fprintf( '(iii) Change spread by +/- inc. For example, spread increase = %.2f \n', 2*inc)
+fprintf( 'Maximum absolute error*e+13 for CRRA utility: %.4f \n', accnonlin*10^13)
+% Maximum percentage deviation
+maxdev = max(abs(ynonlinsp - ylin)./ylin);
+fprintf(  'The maximum percentage deviation is %.2f percent \n', maxdev*100)
+
+function [yact, yapp, yhat] = chebyshev_approx( fun, a, b, m, n, funtype, x)
+% [yact, yapp, yhat] = chebyshev_approx( fun, a, b, m, n, funtype, x)
 % USAGE: Chebychev interpolation 
 % INPUT:
 %     fun  := function handle, e.g., @exp(-x)
@@ -82,12 +141,12 @@ xhat = a + .5*( b - a )*( zhat + 1 );
 %% (3) Evaluate function at Chebyshev nodes
 if strcmp(funtype, 'implicit') % implicit optimal consumption function
     % Plug in xhat for W
-    %calculate actual values of y for x instead of nodes only
+    % Calculate actual values of y for x instead of nodes only
     tmp2 = length(xhat);
-    tmpnew=length(x);
+    tmp3 = length(x);
     yhat = ones(1,tmp2);
-    yhatnew= ones(1,tmpnew);
-    for i = 1:tmpnew
+    yact = ones(1,tmp3);
+    for i = 1:tmp3
        Wnew = x(i);
        myfunnew= @(C0) fun(Wnew,C0);
        x0new=0;
@@ -119,13 +178,18 @@ end
 % Then, polynomial coefficients are given by
 coeff = Tx\yhat';
 
-%evaluate approximation at x
-s = 0;
-for i = 0:n
-    temp = ( 2*( x - a ) )/( b - a ) - 1;
-    Ti = cos( i*acos( temp ) );
-    s = s + coeff(i+1)*Ti;
+%% Evaluate approximation yapp for larger x
+tmp4 = length(x);
+Txnew = ones(tmp4, n+1); % Returns a vector of ones only if n = 0
+if n >= 1          
+    Txnew(:,2) = x';
 end
-yapp = s;
+% Recursively define rest of matrix Tx
+if n >= 2
+    for j = 3:(n+1)
+        Txnew(:,j) = 2*x*Txnew(:,j-1) - Txnew(:,j-2);
+    end
+end
+yapp = Txnew*coeff;
 
 end
